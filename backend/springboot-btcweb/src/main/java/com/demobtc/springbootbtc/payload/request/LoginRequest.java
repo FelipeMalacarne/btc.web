47a3941e525e3dcc71fc1dd3fe8857379a004b59
Felipe Malacarne <105752743+FelipeMalacarne@users.noreply.github.com>
2023-04-15 02:33:59 -0300

    Spring Security RequestPayload

--- backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/payload/request/LoginRequest.java
+++ backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/payload/request/LoginRequest.java
@@ -1,4 +1,16 @@
 package com.demobtc.springbootbtc.payload.request;
 
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+
 }
--- backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/payload/request/SignupRequest.java
+++ backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/payload/request/SignupRequest.java
@@ -1,4 +1,21 @@
 package com.demobtc.springbootbtc.payload.request;
 
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class SignupRequest {
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String email;
+
+    private Set<String> role;
+
+    @NotNull
+    private String password;
+
 }
