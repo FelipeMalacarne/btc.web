7f16d5aa20f20ef462afbf1f466a9fef3d55a49b
Felipe Malacarne <105752743+FelipeMalacarne@users.noreply.github.com>
2023-04-15 02:05:01 -0300

    Spring Security UserDetailsService

--- backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/security/services/UserDetailsServiceImpl.java
+++ backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/security/services/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.demobtc.springbootbtc.security.services;
 
-public class UserDetailsServiceImpl {
+import com.demobtc.springbootbtc.dao.AccountRepository;
+import com.demobtc.springbootbtc.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account = accountRepository.findByAccountName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(account);
+    }
 }
