368e80b5b07917fcbd1baadf7d9fc1064bd889eb
Felipe Malacarne <105752743+FelipeMalacarne@users.noreply.github.com>
2023-04-15 16:31:24 -0300

    Spring Ingredient and Unit entities

--- backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/models/Ingredient.java
+++ backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/models/Ingredient.java
@@ -1,4 +1,32 @@
 package com.demobtc.springbootbtc.models;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "ingredient")
 public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ing_id")
+    private Long id;
+
+    @Column(name = "ing_name")
+    private String name;
+
+    @Column(name = "ing_stock_quanti")
+    private Integer stockQuantity;
+
+    @Column(name = "ing_expiration_date")
+    private String expirationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "un_id")
+    private Unit unitOfMeasure;
+
+
+
 }
