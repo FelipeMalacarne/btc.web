2aa67708784e6fe68ac10abfa1907ac1d927829d
Felipe Malacarne <105752743+FelipeMalacarne@users.noreply.github.com>
2023-04-15 02:35:54 -0300

    Spring Security ResponsePayload

--- backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/payload/response/MessageResponse.java
+++ backend/springboot-btcweb/src/main/java/com/demobtc/springbootbtc/payload/response/MessageResponse.java
@@ -1,4 +1,13 @@
 package com.demobtc.springbootbtc.payload.response;
 
+import lombok.Data;
+
+@Data
 public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
 }
